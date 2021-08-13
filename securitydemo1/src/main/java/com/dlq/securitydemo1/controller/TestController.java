884c18bb7b2e24bf6edc559cf886e961d35488f2
DLQ <1114626450@qq.com>
2021-08-13 10:32:52 +0800

    SpringSecurity 的 Web 权限方案-自定义设置登录页面/接口不需要认证可以访问

--- securitydemo1/src/main/java/com/dlq/securitydemo1/controller/TestController.java
+++ securitydemo1/src/main/java/com/dlq/securitydemo1/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.dlq.securitydemo1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +19,10 @@ public class TestController {
     public String hello(){
         return "hello security";
     }
+
+    @GetMapping("/index")
+    public String index(){
+        return "hello index";
+    }
+
 }
