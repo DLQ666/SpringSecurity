3a044713f470f7830eb792ded9aa3bc34155991e
DLQ <1114626450@qq.com>
2021-08-13 14:35:15 +0800

    SpringSecurity 的 Web 权限方案-hasAnyAuthority 方法能指定多个权限

--- securitydemo1/src/main/java/com/dlq/securitydemo1/config/SecurityConfigDB.java
+++ securitydemo1/src/main/java/com/dlq/securitydemo1/config/SecurityConfigDB.java
@@ -39,7 +39,8 @@ public class SecurityConfigDB extends WebSecurityConfigurerAdapter {
                 .loginProcessingUrl("/user/login") //登录访问路径
                 .defaultSuccessUrl("/test/index").permitAll()// 登录成功之后，跳转路径
                 .and().authorizeRequests().antMatchers("/","/test/hello","/user/login").permitAll() //设置哪些路径可以直接访问，不需要认证
-                .antMatchers("/test/index").hasAuthority("admins")
+                //.antMatchers("/test/index").hasAuthority("admins")
+                .antMatchers("/test/index").hasAnyAuthority("admins,manager")
                 .anyRequest().authenticated()
                 .and().csrf().disable() // 关闭csrf防护
         ;
--- securitydemo1/src/main/java/com/dlq/securitydemo1/service/MyUserDetailService.java
+++ securitydemo1/src/main/java/com/dlq/securitydemo1/service/MyUserDetailService.java
@@ -30,7 +30,7 @@ public class MyUserDetailService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         //调用 userMapper 中方法，根据用户名查询数据库
-        List<GrantedAuthority> auths = AuthorityUtils.commaSeparatedStringToAuthorityList("admins");
+        List<GrantedAuthority> auths = AuthorityUtils.commaSeparatedStringToAuthorityList("manager");
         QueryWrapper<UmsUser> wrapper = new QueryWrapper<>();
         // where username=？
         wrapper.eq("username", username);
